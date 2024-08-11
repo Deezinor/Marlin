3a88becad04a3d9191f6925554a0345cb381b448
Jake Boulton <114814282+Deezinor@users.noreply.github.com>
2024-08-11 14:32:30 +0100

    - set Z probe offset to -1.285 - Machine said -1.28 but looking at a print this needed to be lowered slightly to layer squish and wall adhesion

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1599,7 +1599,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -44, -14.6, 0 } //JMB
+#define NOZZLE_TO_PROBE_OFFSET { -44, -14.6, -1.285 } //JMB
 
 // Enable and set to use a specific tool for probing. Disable to allow any tool.
 #define PROBING_TOOL 0
